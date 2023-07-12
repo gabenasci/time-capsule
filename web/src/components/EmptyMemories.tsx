4efcdcb3b7080610b28aa5ae56c9cbfd1641bd17
gabenasci <gabenascimento1@gmail.com>
2023-07-12 02:35:19 -0300

    Add New Memory page

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -3,7 +3,7 @@ export function EmptyMemories() {
     <div className="flex flex-1 items-center justify-center">
       <p className="text-center leading-relaxed w-[360px]">
         You haven't saved any memories yet,{' '}
-        <a href="" className="underline hover:text-gray-50">start creating one now!</a>!
+        <a href="" className="underline hover:text-gray-50">start creating one now</a>!
       </p>
     </div>
   )
