7fda3e021d0a9332643e3a2303fb297552be3a1c
gabenasci <gabenascimento1@gmail.com>
2023-07-18 11:19:19 -0300

    Add file upload route to serve static image/video files

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,18 @@ import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import fastifyMultipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(fastifyMultipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -18,6 +28,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
