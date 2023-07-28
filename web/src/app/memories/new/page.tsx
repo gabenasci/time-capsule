25b97dc3c66d416a20454ae2ea6380ef45e9134c
gabenasci <gabenascimento1@gmail.com>
2023-07-28 15:00:03 -0300

    Add media picker/preview and upload memory via API

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,7 +1,6 @@
-'use client'
-
 import { MediaPicker } from "@/components/MediaPicker";
-import { Camera, ChevronLeft } from "lucide-react";
+import { NewMemoryForm } from "@/components/NewMemoryForm";
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function NewMemory() {
@@ -12,28 +11,7 @@ export default function NewMemory() {
           return to timeline
       </Link>
 
-      <form className="flex flex-1 flex-col gap-2">
-        <div className="flex items-center gap-4">
-          <label htmlFor="media" className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100">
-            <Camera className="w-4 h-4" />
-            Attach media
-            </label>
-
-            <label htmlFor="isPublic" className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100">
-              <input type="checkbox" name="isPublic" id="isPublic" value="true" className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500" />
-              Make memory public
-            </label>
-        </div>
-
-        <MediaPicker />
-         
-        <textarea
-          name="content"
-          spellCheck={false}
-          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-          placeholder="Feel free to add photos, videos and stories of this experience you'd like to remember forever."
-        />
-      </form>
+      <NewMemoryForm />
     </div>
   )
 }
